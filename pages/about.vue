67b457575f9c52ebc0472130a8f12c89d85a576e
thdat <thdat2706@gmail.com>
2023-05-31 11:06:01 +0700

    123

---
+++ pages/about.vue
@@ -0,0 +1,37 @@
+<template>
+  <h1>about page</h1>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  head() {
+    return {
+      title: "The Pursuit of Happyness - Homepage",
+      meta: [
+        {
+          hid: "og:title",
+          name: "og:title",
+          content: "about title",
+        },
+        {
+          hid: "og:type",
+          name: "og:type",
+          content: "website",
+        },
+        {
+          hid: "og:image",
+          name: "og:image",
+          content:
+            "/favicon.ico",
+        },
+        {
+          hid: "og:description",
+          name: "og:description",
+          content:
+            "about desc" },
+      ],
+    };
+  },
+};
+</script>
