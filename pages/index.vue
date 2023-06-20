8dbc40797ef1ac0fa0917780b0c8a56acf283689
thdat <thdat2706@gmail.com>
2023-06-20 14:47:17 +0700

    1

--- pages/index.vue
+++ pages/index.vue
@@ -41,7 +41,7 @@ export default {
         {
           hid: "og-image",
           property: "og:image",
-          content: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+          content: ImageSrc,
         },
         { hid: "og-desc", property: "og:description", content: "Mee desc" },
         {
@@ -56,7 +56,7 @@ export default {
         {
           hid: "t-image",
           name: "twitter:image",
-          content: 'https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80',
+          content: ImageSrc,
         },
       ],
     };
